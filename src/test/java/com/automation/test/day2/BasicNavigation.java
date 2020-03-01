6dccbb0458e7c14453a0cd15ffd0598f08dde6c0
Seyfullah Yavuz <seyfullahtx@gmail.com>
2020-03-01 12:29:59 -0600

    Created basic navigation

---
+++ src/test/java/com/automation/test/day2/BasicNavigation.java
@@ -0,0 +1,14 @@
+package com.automation.test.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigation {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+    }
+}
